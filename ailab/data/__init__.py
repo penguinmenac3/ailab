cc55b91bfe4a8f8cbf8da7334dae1d226cde4485
Michael Fuerst <mail@michaelfuerst.de>
2019-11-15 23:58:22 +0100

    Removed data and experiment parts which are now integrated in babilim.

--- ailab/data/__init__.py
+++
@@ -1,1 +0,0 @@
-from ailab.data.data_provider import Dataset, TransformedDataset, Transformer, ComposeTransforms
--- ailab/experiment/__init__.py
+++
@@ -1,1 +0,0 @@
-from ailab.experiment.config import Config, ConfigPart, import_config
